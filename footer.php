d6369aa766b54b01adc6a1be6c991f3b16fd6a8b
mayank200529 <rawatmayank685@gmail.com>
2025-08-24 14:40:22 +0530

    added header and fotter files of the project

---
+++ footer.php
@@ -0,0 +1,31 @@
+<script>
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
+</script> 
+<nav style=" background-color:rgb(16, 34, 62);" class="navbar navbar-expand-lg navbar-light ">
+<div class="col text-center text-white p-1">
+	<h5>All copyrights reserved &copy; <?php echo date('Y');?> by Mayank Singh Rawat</h5>
+  <hr style="border: .01cm solid white;">
+</div>
+</nav>
+<nav style="background-color:rgb(16, 34, 62);" class="navbar navbar-expand-lg opaque-navbar" >
+<br>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+  <center>
+    <ul class="navbar-nav">
+    <li class="text-white">
+      <a style="text-decoration:none; color:white; font-size:14px;" href="">Terms of Use</a> - 
+      <a style="text-decoration:none; color:white; font-size:14px;" href="">Accessibility</a> - 
+      <a style="text-decoration:none; color:white; font-size:14px;" href="">Privacy Policy</a> - 
+      <a style="text-decoration:none; color:white; font-size:14px;" href="">Webpage Information</a> - 
+      <a style="text-decoration:none; color:white; font-size:14px;" href="">Ad Choices</a> - 
+      <a style="text-decoration:none; color:white; font-size:14px;" href="">Sitemap</a>
+    </li>
+	  <li class="nav-item">
+      <img style="padding-left:40px; height:25px;" src="assets/img/payment.png">
+    </li>
+    </ul>
+	</center>
+  </div>
+</nav>
